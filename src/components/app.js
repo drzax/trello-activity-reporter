d7b6afb02c8c5d7b02d470bf84812ec4f64b6cc2
Simon Elvery <simon@elvery.net>
2017-08-31 21:20:08 +1000

    Don't auto-popup a login screen

--- src/components/app.js
+++ src/components/app.js
@@ -4,13 +4,15 @@ import { Router } from 'preact-router';
 import Header from './header';
 import Home from '../routes/home';
 import Board from '../routes/board';
-import { authorize } from '../lib/trello';
+import { authorize, setKey } from '../lib/trello';
+
+setKey('ac39b3228640dbb060b760e17b59b4ed');
 
 // import Home from 'async!./home';
 // import Profile from 'async!./profile';
 
 export default class App extends Component {
-	
+
 	/** Gets fired when the route changes.
 	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
 	 *	@param {string} event.url	The newly routed URL
@@ -18,17 +20,39 @@ export default class App extends Component {
 	handleRoute = e => {
 		this.currentUrl = e.url;
 	};
-	
-	componentDidMount() {
-		authorize('ac39b3228640dbb060b760e17b59b4ed')
-			.then(token => {
+
+	handleAuth(e) {
+		e.preventDefault();
+
+		authorize().then(
+			token => {
 				this.setState({ token });
-			}, err => {
+			},
+			err => {
 				this.setState({ err });
-			});
+			}
+		);
 	}
-	
+
+	constructor() {
+		super();
+		this.handleAuth = this.handleAuth.bind(this);
+	}
+
+	componentWillMount() {
+		this.setState({
+			token: window.localStorage.trelloToken
+		});
+	}
+
 	render(props, state) {
+		if (!this.state.token) {
+			return (
+				<div id="app">
+					<a href="#" onClick={this.handleAuth}>{`Login with Trello`}</a>
+				</div>
+			);
+		}
 		return (
 			<div id="app">
 				<Header />
--- src/lib/trello.js
+++ src/lib/trello.js
@@ -1,24 +1,29 @@
 const authEndpoint = 'https://trello.com';
-const apiEndpoint = "https://api.trello.com";
+const apiEndpoint = 'https://api.trello.com';
 const version = '1';
 const w = window;
 let key;
 
-export { authorize, get };
+export { authorize, get, setKey };
 
-function authorize (k) {
-	
+function setKey(k) {
 	key = k;
-	
-	return new Promise((resolve, reject) => {
+}
 
+function authorize(k) {
+	if (k) setKey(k);
+
+	return new Promise((resolve, reject) => {
 		if (window.localStorage.trelloToken) {
 			return resolve(window.localStorage.trelloToken);
 		}
-		
-		let authUrl = `${authEndpoint}/${version}/authorize?response_type=token&key=${key}&return_url=${encodeURIComponent(w.location.href)}&callback_method=postMessage&scope=read%2Cwrite&expiration=never&name=Torbreck%20Reports`;
+
+		let authUrl = `${authEndpoint}/${version}/authorize?response_type=token&key=${key}&return_url=${encodeURIComponent(
+			w.location.href
+		)}&callback_method=postMessage&scope=read%2Cwrite&expiration=never&name=Torbreck%20Reports`;
 		let name = 'trello';
-		let config = `width=420,height=470,left=${w.screenX + (w.innerWidth - 420) / 2},top=${w.screenY + (w.innerHeight - 470) / 2}`;
+		let config = `width=420,height=470,left=${w.screenX +
+			(w.innerWidth - 420) / 2},top=${w.screenY + (w.innerHeight - 470) / 2}`;
 		let popup = w.open(authUrl, name, config);
 		let listener = msg => {
 			// Check this message is from trello
@@ -29,18 +34,16 @@ function authorize (k) {
 					w.localStorage.trelloToken = msg.data;
 					return resolve(msg.data);
 				}
-				
+
 				reject(new Error('Unexpected message from Trello'));
 			}
 		};
-		
+
 		w.addEventListener('message', listener);
-		
 	});
-	
 }
 
-function get(path, params={}) {
+function get(path, params = {}) {
 	const token = window.localStorage.trelloToken;
 	params.token = token;
 	params.key = key;
@@ -49,5 +52,7 @@ function get(path, params={}) {
 }
 
 function toQueryString(obj) {
-	return Object.keys(obj).map((k) => encodeURIComponent(k) + '=' + encodeURIComponent(obj[k])).join('&');
+	return Object.keys(obj)
+		.map(k => encodeURIComponent(k) + '=' + encodeURIComponent(obj[k]))
+		.join('&');
 }
