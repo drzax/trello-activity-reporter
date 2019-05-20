3d786a7184dcbe7db3cc4832e4f7af49ed582d8c
Simon Elvery <simon@elvery.net>
2019-05-20 22:20:52 +1000

    Improve display of boards on home screen

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import styles from "./styles.scss";
 import { get } from "../../lib/trello";
 import { navigate } from "@reach/router";
+import { subMonths, isAfter } from "date-fns";
 
-import Card from "@material-ui/core/Card";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
 export default function Home() {
@@ -25,19 +25,64 @@ export default function Home() {
   if (!boards) {
     return <CircularProgress />;
   }
-  console.log("boards", boards);
+
+  const starredLabel = "Starred Boards";
+  const privateLabel = "Personal Boards";
+  const recentLabel = "Recently Viewed";
+  const recentSince = subMonths(new Date(), 1);
+
+  const orgs = Array.from(
+    boards.reduce((map, board) => {
+      const orgId = board.idOrganization || privateLabel;
+      const orgArr = map.get(orgId) || [];
+
+      orgArr.push(board);
+      map.set(orgId, orgArr);
+
+      if (board.starred) {
+        const arr = map.get(starredLabel) || [];
+        arr.push(board);
+        map.set(starredLabel, arr);
+      }
+
+      if (
+        board.dateLastView &&
+        isAfter(new Date(board.dateLastView), recentSince)
+      ) {
+        const arr = map.get(recentLabel) || [];
+        arr.push(board);
+        map.set(recentLabel, arr);
+      }
+
+      return map;
+    }, new Map([[starredLabel, []], [recentLabel, []], [privateLabel, []]]))
+  );
+
   return (
-    <div className={styles.list}>
-      {boards.map(board => (
-        <div
-          style={{ backgroundColor: board.prefs.backgroundColor }}
-          className={styles.listItem}
-          id={board.id}
-          key={board.id}
-          onClick={() => navigate(`/board/${board.id}`)}
-        >
-          <h1>{board.name}</h1>
-          <p>{board.organization ? board.organization.displayName : null}</p>
+    <div>
+      {orgs.map(([org, boards]) => (
+        <div className={styles.orgs}>
+          <h1>
+            {boards[0] && boards[0].idOrganization === org
+              ? boards[0].organization.displayName
+              : org}
+          </h1>
+          <div className={styles.list}>
+            {boards.map(board => (
+              <div
+                style={{ backgroundColor: board.prefs.backgroundColor }}
+                className={styles.listItem}
+                id={board.id}
+                key={board.id}
+                onClick={() => navigate(`/board/${board.id}`)}
+              >
+                <h1>{board.name}</h1>
+                <p>
+                  {board.organization ? board.organization.displayName : null}
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
       ))}
     </div>
